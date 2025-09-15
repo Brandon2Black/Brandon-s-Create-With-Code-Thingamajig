71453010bdc8dcf5caba7fc969c6fd1dfcccacb0
Brandon2Black <brandonb627@nycstudents.net>
2025-09-15 11:59:53 -0400

    .

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 12, -24);
+    private Vector3 offset = new Vector3(0, 11, -25);
     // Start is called before the first frame update
     void Start()
     {
